cffe661d0d30e4d8ce9e080dbdce2def15703f8e
juststop <69150118+juststopp@users.noreply.github.com>
2021-04-07 14:24:14 +0200

    Update success-router.js

--- dashboard/routers/success-router.js
+++ dashboard/routers/success-router.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
 const ms = require('ms');
 const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
-    'mode': 'live',
-    'client_id': 'AUidZSC5g9XquT_c34MEPUi5y65PR0uuNjZ2rve2smEZHj3A9dYeL2Cmpu-0Y9wYostcqzrdPBTVTDh_',
-    'client_secret': 'EK098sD4Q2GzLDJf8py9oQ0w-yxqaaxT3AjP_I73YelgOfiGPMcyM2o9G6PUiuqcasUOuSR9t6a-DeTs'
+    'mode': process.env.PAYPAL_MODE,
+    'client_id': process.env.PAYPAL_ID,
+    'client_secret': process.env.PAYPAL_SECRET
 });
 
 router.get('/', async (req, res, next) => {
